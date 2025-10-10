655a7149f0ae32e01599119d78a7ef89a98a15eb
Ian <ianfvolkmuth@gmail.com>
2025-10-09 18:32:42 -0700

    Started setting up ArcadeMotor command and motor subsystem

---
+++ frc-training-2025/frc-training-2025/src/main/java/frc/robot/commands/ArcadeMotor.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Motor;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class ArcadeMotor extends Command {
+  /** Creates a new ArcadeMotor. */
+  private Motor m_motor;
+  private Joystick m_Joystick;
+  public ArcadeMotor(Joystick joystick, Motor motor) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    double m_speed = 0;
+    
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
