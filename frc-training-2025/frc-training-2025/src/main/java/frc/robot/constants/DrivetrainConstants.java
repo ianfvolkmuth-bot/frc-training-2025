6906857b7cb03238160b8daf59282d528d4143d3
Ian <ianfvolkmuth@gmail.com>
2025-10-16 17:36:29 -0700

    Update, some arcade-drive files that transferred deleted

---
+++ frc-training-2025/frc-training-2025/src/main/java/frc/robot/constants/DrivetrainConstants.java
@@ -0,0 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.constants;
+
+/** Add your docs here. */
+public class DrivetrainConstants {
+    public static final int kLeftPrimaryMotorID = 0;
+    public static final int kRightPrimaryMotorID = 1;
+    
+    public static final int kLeftSecondaryMotorID = 3;
+    public static final int kRightSecondaryMotorID = 4;
+    
+    public static final double kJoystickInputMultiplier = 0.5;
+}
